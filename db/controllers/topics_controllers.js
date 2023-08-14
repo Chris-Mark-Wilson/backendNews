80e3e1a5073c224670f50068871a4d9a97795bfe
bigsegs <cw.plastering.services@gmail.com>
2023-08-14 11:42:38 +0100

    add new describe for general 404 bad route, send 200 for no topics

--- db/controllers/topics_controllers.js
+++ db/controllers/topics_controllers.js
@@ -1,15 +1,13 @@
-const selectAllTopics=require('../models/topics_models')
+const selectAllTopics = require("../models/topics_models");
 
-const getAllTopics=((req,res,next)=>{
-selectAllTopics()
-.then((result)=>{
-    
-res.status(200).send({topics:result})
-})
-.catch((err)=>{
-    next(err)
-})
-
- 
-})
-module.exports=getAllTopics;
+const getAllTopics = (req, res, next) => {
+  selectAllTopics()
+    .then((result) => {
+      if (result.length === 0) res.status(200).send("No topics found");
+      res.status(200).send({ topics: result });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+module.exports = getAllTopics;
--- db/models/topics_models.js
+++ db/models/topics_models.js
@@ -5,9 +5,7 @@ const selectAllTopics=()=>{
     return db.query(`
     SELECT * FROM topics;`)
     .then(({rows})=>{
-        if(rows.length===0){
-            return Promise.reject({status:404,msg:"not found"})
-        }
+      
         return rows;
     })
     .catch((err)=>{
