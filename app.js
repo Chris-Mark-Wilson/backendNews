68048d0cb3d223e01d188d7e4ec0c1e850aa8a5a
bigsegs <cw.plastering.services@gmail.com>
2023-08-18 13:47:49 +0100

    add comments router

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
 const express = require("express");
 
+const { customErrors, sqlErrors, serverErrors } = require("./db/errors");
 
-const { customErrors, sqlErrors,serverErrors } = require("./db/errors");
-
-const {
-  getCommentsByArticleId,postComment,removeComment
-} = require("./controllers/comments_controllers");
-const getUsers=require('./controllers/users_controllers')
-const apiRouter = require('./routes/apiRouter');
+const apiRouter = require("./routes/apiRouter");
 const articleRouter = require("./routes/articlesRouter");
-const topicRouter=require('./routes/topicsRouter');
+const topicRouter = require("./routes/topicsRouter");
 const userRouter = require("./routes/usersRouter");
-const commentRouter=require('./routes/commentsRouter')
-
+const commentRouter = require("./routes/commentsRouter");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api', apiRouter);
-app.use('/api/articles',articleRouter)
-app.use('/api/topics',topicRouter)
-app.use('/api/users',userRouter)
-app.use('api/comments',commentRouter)
-
-
-
-
-
-
-
-
-
-
-
-app.delete('/api/comments/:comment_id',removeComment)
-
-
+app.use("/api", apiRouter);
+app.use("/api/articles", articleRouter);
+app.use("/api/topics", topicRouter);
+app.use("/api/users", userRouter);
+app.use("/api/comments", commentRouter);
 
 app.use((_, res) => {
   res.status(404).send({ status: 404, msg: "Path not found" });
 });
 
 app.use(customErrors);
-app.use(sqlErrors)
+app.use(sqlErrors);
 app.use(serverErrors);
 
 module.exports = app;
