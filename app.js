f8754e00aaebd1322d365f313773533532f63fc4
bigsegs <cw.plastering.services@gmail.com>
2023-08-18 12:35:32 +0100

    add articles router,api router

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 const express = require("express");
 const getAllTopics = require("./controllers/topics_controllers");
-const {
-  getArticleById,
-  getAllArticles,
-  patchArticle,
-} = require("./controllers/articles_controllers");
+
 const { customErrors, sqlErrors,serverErrors } = require("./db/errors");
-const getAllEndpoints = require("./controllers/endpoints_controller");
+
 const {
   getCommentsByArticleId,postComment,removeComment
 } = require("./controllers/comments_controllers");
 const getUsers=require('./controllers/users_controllers')
+const apiRouter = require('./routes/apiRouter');
+const articleRouter = require("./routes/articlesRouter");
+
 
 const app = express();
 app.use(express.json())
 
-app.get("/api", getAllEndpoints);
+app.use('/api', apiRouter);
+app.use('/api/articles',articleRouter)
+
+
+
+
 
 app.get("/api/topics", getAllTopics);
 
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles", getAllArticles);
-
 app.get("/api/users",getUsers)
 
-app.post('/api/articles/:article_id/comments',postComment)
+
 
 app.delete('/api/comments/:comment_id',removeComment)
 
-app.patch("/api/articles/:article_id", patchArticle);
+
 
 app.use((_, res) => {
   res.status(404).send({ status: 404, msg: "Path not found" });
---
+++ routes/apiRouter.js
@@ -0,0 +1,7 @@
+const apiRouter = require('express').Router();
+const getAllEndpoints = require("../controllers/endpoints_controller");
+
+apiRouter.get('/',getAllEndpoints)
+
+
+module.exports = apiRouter;
