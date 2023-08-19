4708e358f4623f10c648b2bdc84623d9f767e9ca
bigsegs <cw.plastering.services@gmail.com>
2023-08-19 18:40:15 +0100

    update endpoints.json

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -747,7 +747,7 @@ describe("POST /api/articles", () => {
             votes: 0,
             topic: "mitch",
             article_img_url:
-              "https://media.istockphoto.com/id/508273083/vector/newspaper.jpg?s=612x612&w=0&k=20&c=07dOAo-KtyY92hRMJeIrp5BBDs3gXKGz3Fjf-sJh_JE=",
+              "https://media.istockphoto.com/id/508273083/vector/newspaper.jpg?s=612x612&w=0&k=20&c=07dOAo-KtyY92hRMJeIrp5BBDs3gXKGz3Fjf-sJh_JE="
           })
         );
       })
